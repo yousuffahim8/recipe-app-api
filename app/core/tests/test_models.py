2c8bbb58a63cf633194926b4aa4ed141da36d82f
Yousuf <yousuffahim8@gmail.com>
2020-06-21 06:46:32 -0400

    Added image field

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from core import models
+from unittest.mock import patch
 
 def sample_user(email='test@londonappdev.com', password='testpass'):
     """Create a sample user"""
@@ -72,6 +73,18 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
+
 
 
 #    def test_user_page_change(self):
